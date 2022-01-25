c6658a80b57457b8ff09a0ec1efc3c8ff81eb394
Blinky <captinblinky@gmail.com>
2022-01-24 21:32:26 -0500

    Music Commands

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-//const botSettings = require("./botsettings.json");
-//const fs = require("fs");
-//const prefix = botSettings.prefix;
 const Discord = require("discord.js");
 require("dotenv").config()
+const { Distube } = require("distube");
+const { SpotifyPlugin } = require("@distube/spotify");
+
 
 const client = new Discord.Client({
     intents: [
@@ -22,6 +22,14 @@ let bot = {
 client.commands = new Discord.Collection();
 client.events = new Discord.Collection();
 
+client.Distube = new Distube(client, {
+    emitNewSongOnly: true,
+    emitAddSongWhenCreatingQueue: false,
+    plugins: [new SpotifyPlugin()]
+});
+
+module.exports = client;
+
 client.loadEvents = (bot, reload) => require("./handlers/events")(bot, reload);
 client.loadCommands = (bot, reload) => require("./handlers/commands")(bot, reload);
 
@@ -29,45 +37,5 @@ client.loadEvents(bot, false);
 client.loadCommands(bot, false);
 
 module.exports = bot
-
-// fs.readdir("./cmds/", (err, files) => {
-//     if (err) console.error(err);
-
-//     let jsfiles = files.filter(f => f.split(".").pop() === "js");
-//     if (jsfiles.length <= 0) {
-//         console.log("No commands to load.");
-//         return;
-//     }
-//     console.log(`Loading ${jsfiles.length} commands!`);
-
-//     jsfiles.forEach((f, i) => {
-//         let props = require(`./cmds/${f}`);
-//         console.log(`${i + 1}: ${f} loaded`);
-//         bot.commands.set(props.help.name, props);
-//     });
-// });
-
-// bot.on("ready", async() => {
-//     console.log(`Logged in as ${bot.user.tag} and ready to game~`);
-
-//     /* try {
-//          let link = await bot.generateInvite(["ADMINISTRATOR"]);
-//          console.log(`Link for bot: ${link}`);
-//      } catch (e) {
-//          console.log(e.stack);
-//      }*/
-// });
-
-// bot.on("messageCreate", (message) => {
-//     if (message.author.bot) return;
-
-//     let messageArray = message.content.split(" ");
-//     let command = messageArray[0];
-//     let args = messageArray.slice(1);
-
-//     let cmd = bot.commands.get(command.slice(prefix.length));
-//     if (cmd) cmd.run(bot, message, args);
-
-// });
 
 client.login(process.env.TOKEN);
